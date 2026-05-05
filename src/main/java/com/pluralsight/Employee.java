42751e57aa8dcc4be3f4ed661a1e402e9f82630f
birukbelaytafese <birukbelay175@gmail.com>
2026-05-05 12:03:51 -0500

    Hotel Operation Commit

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -1,4 +1,64 @@
 package com.pluralsight;
 
 public class Employee {
+
+    private int employeeId;
+    private String name;
+    private String department;
+    private double payRate;
+    private double hoursWorked;
+
+    // Constructor (THIS WAS MISSING)
+    public Employee(int employeeId, String name, String department, double payRate, double hoursWorked) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    // Getters (THIS WAS MISSING)
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    // Derived methods
+    public double getRegularHours() {
+        if (hoursWorked <= 40) {
+            return hoursWorked;
+        } else {
+            return 40;
+        }
+    }
+
+    public double getOvertimeHours() {
+        if (hoursWorked > 40) {
+            return hoursWorked - 40;
+        } else {
+            return 0;
+        }
+    }
+
+    public double getTotalPay() {
+        double regularPay = getRegularHours() * payRate;
+        double overtimePay = getOvertimeHours() * payRate * 1.5;
+
+        return regularPay + overtimePay;
+    }
 }
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,42 @@
 package com.pluralsight;
 
 public class Main {
+    public static void main(String[] args) {
 
+        // -------- ROOM TEST --------
+        Room room = new Room(2, 124.00, false, false);
+
+        System.out.println("ROOM TEST");
+        System.out.println("Beds: " + room.getNumberOfBeds());
+        System.out.println("Price: $" + room.getPrice());
+        System.out.println("Occupied: " + room.isOccupied());
+        System.out.println("Dirty: " + room.isDirty());
+        System.out.println("Available: " + room.isAvailable());
+
+        System.out.println();
+
+        // -------- RESERVATION TEST --------
+        Reservation reservation = new Reservation("king", 3, true);
+
+        System.out.println("RESERVATION TEST");
+        System.out.println("Room Type: " + reservation.getRoomType());
+        System.out.println("Price per night: $" + reservation.getPrice());
+        System.out.println("Nights: " + reservation.getNumberOfNights());
+        System.out.println("Weekend: " + reservation.isWeekend());
+        System.out.println("Total: $" + reservation.getReservationTotal());
+
+        System.out.println();
+
+        // -------- EMPLOYEE TEST --------
+        Employee emp = new Employee(1, "John", "IT", 20.0, 45);
+
+        System.out.println("EMPLOYEE TEST");
+        System.out.println("Name: " + emp.getName());
+        System.out.println("Department: " + emp.getDepartment());
+        System.out.println("Pay Rate: $" + emp.getPayRate());
+        System.out.println("Hours Worked: " + emp.getHoursWorked());
+        System.out.println("Regular Hours: " + emp.getRegularHours());
+        System.out.println("Overtime Hours: " + emp.getOvertimeHours());
+        System.out.println("Total Pay: $" + emp.getTotalPay());
+    }
 }
